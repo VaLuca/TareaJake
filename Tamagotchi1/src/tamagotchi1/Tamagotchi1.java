c8f5ff151c9195e98b6a399afc041bba7494408f
Valeria <val.sunshine12@gmail.com>
2015-10-07 00:06:25 -0500

    Tamagotchi

---
+++ Tamagotchi1/src/tamagotchi1/Tamagotchi1.java
@@ -0,0 +1,17 @@
+package tamagotchi1;
+
+/**
+ *
+ * @author val
+ */
+public class Tamagotchi1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vista.ejecutar();
+    }
+    
+}
