95ab97047b0c0244937b9ded9c64f20756436912
Valeria <val.sunshine12@gmail.com>
2015-10-07 01:32:20 -0500

    calculadora

---
+++ Calculadora/src/calculadora/calculadoraBasica.java
@@ -0,0 +1,16 @@
+package calculadora;
+
+/**
+ *
+ * @author val
+ */
+public class calculadoraBasica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vista.ejecutar();
+    }
+    
+}
